f08495ac8e070df5ddedc9a663ee0c659fdff71c
Phuc Pham <phucpv62@wru.vn>
2021-03-21 16:46:27 +0700

    Connect to mongodb

--- src/module/index/index.controller.ts
+++ src/module/index/index.controller.ts
@@ -3,7 +3,7 @@ import {Request, Response, NextFunction } from "express";
 export default class IndexCotroller {
     public index = (req: Request, res: Response, next: NextFunction) => {
         try {
-            res.status(200).send("API is running ...");
+            res.status(200).send("API is running ahihi...");
         } catch (error) {
             next(error);    //đưa đến midlleware tiếp theo xử lý lỗi.
         }
